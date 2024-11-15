5508e998eb57f2fec768c710578da8e05be73809
Rafael Vareto <rafaelvareto@dcc.ufmg.br>
2024-11-15 17:10:13 -0300

    Adding unit test for DTL

--- python/segloss/tests/test_loss.py
+++ python/segloss/tests/test_loss.py
@@ -16,7 +16,7 @@ def test_abl():
             if idy > idx+1: label[:,:,idy,idx] = 1
             if idy < idx-1: label[:,:,idy,idx] = 2
 
-    criterion  =  segloss.ActiveBoundaryLoss(border_params=[0.1,'cpu',True,0.2,1/100,20.])
+    criterion  = segloss.ActiveBoundaryLoss(border_params=[0.1,'cpu',True,0.2,1/100,20.])
     loss_score = criterion(image, label)
     loss_score.backward()
 
@@ -32,7 +32,7 @@ def test_bfl():
             if idy > idx+1: label[:,:,idy,idx] = 1
             if idy < idx-1: label[:,:,idy,idx] = 2
 
-    criterion  =  segloss.BoundaryScoreLoss(border_params=[3,5,0.5], reduction='sum')
+    criterion  = segloss.BoundaryScoreLoss(border_params=[3,5,0.5], reduction='sum')
     loss_score = criterion(image, label)
     loss_score.backward()
 
@@ -48,9 +48,25 @@ def test_cel():
             if idy > idx+1: label[:,:,idy,idx] = 1
             if idy < idx-1: label[:,:,idy,idx] = 2
 
-    criterion  =  segloss.CrossEntropyLoss(reduction='sum')
+    criterion  = segloss.CrossEntropyLoss(reduction='sum')
     loss_score = criterion(image, label)
     loss_score.backward()
 
     assert round(loss_score.item(), 3) == None # Replace value
 
+
+
+def test_dtl():
+    image = torch.rand([1, 3, 64, 64])
+    label = torch.zeros([1, 1, 64, 64], dtype=torch.long)
+    
+    for idy in range(image.shape[2]):
+        for idx in range(image.shape[3]):
+            if idy > idx+1: label[:,:,idy,idx] = 1
+            if idy < idx-1: label[:,:,idy,idx] = 2
+
+    criterion  = segloss.DistanceTransformLoss(border_params=[1,0.5,1.0])
+    loss_score = criterion(image, label)
+    loss_score.backward()
+
+    assert round(loss_score.item(), 3) == None # Replace value
